acaa1ca069210c39fc3fe1c5185e1804cf2228c6
Rob Chartier <rob.chartier@ethersolutions.ca>
2022-11-27 17:53:42 -0800

    run can take optional args

--- run.sh
+++ run.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 # docker container run -ti robchartier/zshk8s
-docker container run \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v ~/.ssh:/root/.ssh \
-	-v ~/.kube/:/root/.kube \
-	-v ~/source:/root/source \
-	-it robchartier/zshk8s
+if [ $# -eq 0 ]
+  then
+	docker container run \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		-v ~/.ssh:/root/.ssh \
+		-v ~/.kube/:/root/.kube \
+		-v ~/source:/root/source \
+		-it robchartier/zshk8s
+
+else
+	docker container run \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		-v ~/.ssh:/root/.ssh \
+		-v ~/.kube/:/root/.kube \
+		-v ~/source:/root/source \
+		$@ \
+		-it robchartier/zshk8s
+
+
+fi
+
