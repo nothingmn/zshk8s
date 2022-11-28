9bd7121aaee28b86635d935a4df6d901a9d9515a
Rob Chartier <rob.chartier@ethersolutions.ca>
2022-11-27 16:38:41 -0800

    flatten the image before publishing

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker login
+docker run  --name zshk8s_export robchartier/zshk8s
+docker export zshk8s_export | docker import - robchartier/zshk8s:latest
 docker push robchartier/zshk8s
